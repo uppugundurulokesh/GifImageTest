10275c4a5015d8024019b7660d2bb84cc3b48f79
Kanakamma <lokesh26.apssdc@gmail.com>
2018-12-29 22:56:58 +0530

    Initial commit

---
+++ app/src/main/java/com/kanakamma/gifimagetest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kanakamma.gifimagetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
